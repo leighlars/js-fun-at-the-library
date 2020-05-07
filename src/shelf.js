07b5b62eed2a59b9bb349b335aa237bb009347b1
Leigh Larson <leighlarson4@gmail.com>
2020-05-06 23:25:15 -0600

    Completed shelf exercises

--- src/shelf.js
+++ src/shelf.js
@@ -1,8 +1,33 @@
+function shelfBook(book, scifiShelf) {
+  if (scifiShelf.length < 3) {
+    scifiShelf.unshift(book)
+  }
+}
 
+function unshelfBook(removedBookTitle, scifiShelf) {
+  var i = scifiShelf.findIndex(book => book.title === removedBookTitle);
+  scifiShelf.splice(i, 1);
+}
+
+function listTitles(fantasyShelf) {
+  var titles = [];
+  for (var i = 0; i < fantasyShelf.length; i++) {
+    titles.push(fantasyShelf[i].title);
+  }
+  return titles.join(", ");
+}
+
+function searchShelf(scifiShelf, bookTitle) {
+  if (scifiShelf.find(book => book.title === bookTitle)) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 module.exports = {
-  // shelfBook: shelfBook,
-  // unshelfBook: unshelfBook,
-  // listTitles: listTitles,
-  // searchShelf: searchShelf
+  shelfBook: shelfBook,
+  unshelfBook: unshelfBook,
+  listTitles: listTitles,
+  searchShelf: searchShelf
 };
